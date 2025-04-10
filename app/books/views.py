51d70d18c2a78702eddae1f861c5296a977fc8f2
Andrea <andrearaberabe@gmail.com>
2025-04-10 21:59:56 +0300

    make up CRUD for books

--- app/books/views.py
+++ app/books/views.py
@@ -7,9 +7,14 @@ from app.books.schema import BooksType
 class Query(graphene.ObjectType):
     all_books = graphene.List(BooksType)
 
+    search_books = graphene.List(BooksType, title=graphene.String(required=True))
+
     #  get all books
     def resolve_all_books(root, info):
         return Books.objects.all()
+
+    def resolve_search_books(root, info, title):
+        return Books.objects.filter(title__icontains=title)
 
 
 class CreateBook(graphene.Mutation):
@@ -25,8 +30,50 @@ class CreateBook(graphene.Mutation):
         return CreateBook(book=book)
 
 
+class UpdateBook(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        title = graphene.String()
+        excerpt = graphene.String()
+
+    book = graphene.Field(BooksType)
+
+    def mutate(self, info, id, title=None, excerpt=None):
+        try:
+            book = Books.objects.get(pk=id)
+        except Books.DoesNotExist:
+            raise Exception("Book not found")
+
+        if title is not None:
+            book.title = title
+        if excerpt is not None:
+            book.excerpt = excerpt
+
+        book.save()
+        return UpdateBook(book=book)
+
+
+class DeleteBook(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    ok = graphene.Boolean()
+    book = graphene.Field(BooksType)
+
+    def mutate(self, info, id):
+        try:
+            book = Books.objects.get(pk=id)
+        except Books.DoesNotExist:
+            raise Exception("Book not found")
+
+        book.delete()
+        return DeleteBook(ok=True, book=book)
+
+
 class Mutation(graphene.ObjectType):
     create_book = CreateBook.Field()
+    update_book = UpdateBook.Field()
+    delete_book = DeleteBook.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
